733bfd0dbb0e74bf5010a327601bcf737575da5f
Noah Endo <noah.eyeing@gmail.com>
2024-02-16 11:51:57 +0900

    put the database.yml file back to default

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    bookmark_hash = bookmark_params
+    bookmark_hash[:list_id] = params[:list_id]
+    @bookmark = Bookmark.new(bookmark_hash)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list_id)
+  end
+
+  private
+
+  def bookmark_params
+   params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
